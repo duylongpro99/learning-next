6cbe48b7420504bf594bcb24d7a68f4490b42749
long.dao <long.dao@VN-ANNGO>
2021-11-29 16:00:56 +0700

    readFiles in directory and show in route people

---
+++ pages/people/index.tsx
@@ -0,0 +1,50 @@
+import path from "path";
+import { promises as fs } from "fs";
+
+export type People = {
+    id: number;
+    lastName: string;
+    firstName: string;
+    age: number;
+}
+
+type ComponentProps = {
+    people: People[][];
+}
+
+export default function Pepole(props: ComponentProps) {
+
+    return (<div>
+        {props && props.people && props.people[0].map((person) => {
+            return (
+                <p key={person.id}>{person.firstName + ' ' + person.lastName}</p>
+            );
+        })}
+    </div>);
+}
+
+export async function getStaticProps() {
+    const peopleJsonDirectory = path.join(process.cwd(), 'mock');
+    const fileNames = await fs.readdir(peopleJsonDirectory);
+    const _people = fileNames.map(async (fileName) => {
+        const filePath = path.join(peopleJsonDirectory, fileName);
+        const fileContents = await fs.readFile(filePath, 'utf-8');
+        const people: People[] = [];
+        let peopleObjs = JSON.parse(fileContents);
+        let peopleNames = Object.keys(JSON.parse(fileContents));
+        peopleNames.map((name, i) => {
+            people.push({
+                firstName: name,
+                id: i+1,
+                lastName: peopleObjs[name].lastName,
+                age: peopleObjs[name].age
+            });
+        });
+        return people; 
+    });
+    return {
+        props: {
+            people: await Promise.all(_people)
+        }
+    }
+}
