8bea9ea88e74744c0ce6940db0adafa52a239791
long.dao <long.dao@VN-ANNGO>
2021-11-30 15:25:27 +0700

    Type for react function component

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,20 @@
+import React from "react"
+
+type FooterText = {
+    content: string;
+}
+
+const FooterText: React.FC<FooterText> = ( {content} ) => {
+    return (<p>
+        {content}
+    </p>)
+}
+
 export default function Footer() {
     return (
         <div>
             Footer
+            <FooterText content={'Who am i?'} />
         </div>
     )
 }
