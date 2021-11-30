eef0c557536392ec8a5ea2a8b64dcc9e5e0aefb0
long.dao <long.dao@VN-ANNGO>
2021-11-30 15:11:47 +0700

    childern is React.ReactNode type

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,7 +1,8 @@
+import React from "react";
 import Footer from "../footer";
 import NavBar from "../navbar";
 
-export default function Layout({ children }: {children: any}) { 
+export default function Layout({ children }: {children: React.ReactNode}) { 
     return  (
         <>
             <NavBar />
